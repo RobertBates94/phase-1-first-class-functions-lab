03b3cab8adf93e4d797be183fb943a705c9a596b
Robert <bates.to.infinity@gmail.com>
2021-10-13 23:05:42 -0500

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const returnFirstTwoDrivers = (array) => array.slice(0, 2);
 
 const returnLastTwoDrivers = (array) => array.slice(-2);
 
-selectingDrivers (returnFirstTwoDrivers(), returnLastTwoDrivers())
+function selectingDrivers () {
+    
+} 
