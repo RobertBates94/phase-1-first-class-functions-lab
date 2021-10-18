105522101c0d040587505cb0a576ea84b1047cb8
Robert <bates.to.infinity@gmail.com>
2021-10-18 18:12:54 -0500

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const returnFirstTwoDrivers = (array) => array.slice(0, 2);
 
 const returnLastTwoDrivers = (array) => array.slice(-2);
 
-function selectingDrivers () {
-    
-} 
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (fare) {
+    return function (fareMultiplier) {
+        return  fareMultiplier * fare
+    }
+}
+
+function fareDoubler (double, createFareMultiplier, fare) {
+    return fare * double
+}
